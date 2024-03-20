2dc14e852dacfa7967a05bbe1b42523ff7e4f170
Konstantin Pankratov <konstantin.pankratov@live.com>
2024-03-20 11:27:45 -0400

    Added check for wrong path or already existing backup file.

--- Backup.ps1
+++ Backup.ps1
@@ -15,6 +15,18 @@ Param(
     [string]$DestinationPath = './'
 )
 
+If(-Not(Test-Path $SourcePath))
+{
+    Throw "The source directory $SourcePath does not exist, please specify an existing directory."
+}
+
 $date = Get-Date -format "yyyy-MM-dd"
-Compress-Archive -Path $SourcePath -CompressionLevel 'Fastest' -DestinationPath "$($DestinationPath + '/backup-' + $date)"
-Write-Host "Created backup at $($DestinationPath + './backup-' + $date + '.zip')"
+$destinationFile = "$($DestinationPath + './backup-' + $date + '.zip')"
+
+If (-Not (Test-Path $destinationFile)) 
+{
+  Compress-Archive -Path $SourcePath -CompressionLevel 'Fastest' -DestinationPath "$($DestinationPath + 'backup-' + $date)"
+  Write-Host "Created backup at $($DestinationPath + 'backup-' + $date + '.zip')"
+} Else {
+  Write-Error "Today's backup already exists"
+}
